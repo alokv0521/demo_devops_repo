63006c35c06ad6cc46fb653a7bf833d009a60c30
pyaracetamol_143mg <alokv0521@gmail.com>
2026-03-07 20:43:38 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define loop(m,n) for(int i=m; i<n;i++)
+#define vin vector<int> 
+
+
+
+int main()
+{
+
+cout<<"this is the demo repo for devops";
+
+
+
+return 0;
+}

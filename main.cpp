d8b02c98f66ab40cccb1f1269c01017fbd442f87
pyaracetamol_143mg <alokv0521@gmail.com>
2026-03-07 20:54:13 +0530

    add function added

--- main.cpp
+++ main.cpp
@@ -3,14 +3,16 @@ using namespace std;
 #define loop(m,n) for(int i=m; i<n;i++)
 #define vin vector<int> 
 
-
+void add(int a, int b ){
+    cout<<a+b;
+}
 
 int main()
 {
 
 cout<<"this is the demo repo for devops";
 
-
+add(5, 7);
 
 return 0;
 }
